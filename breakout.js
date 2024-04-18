aab0dc65fcf9ed70a9a32aa1ff2d66b2da08c27e
drabekam <drabekam@mail.uc.edu>
2024-04-17 22:14:19 -0400

    Adding in paddle items

--- breakout.js
+++ breakout.js
@@ -19,27 +19,46 @@ const config = {
         },
     }
 };
-
-
-
-let ball, yellowBricks, redBricks;
-
-
+ 
+ 
+ 
+let ball, yellowBricks, redBricks, paddle;
+ 
+ 
 const game = new Phaser.Game(config);
-
-
+ 
+ 
 function preload() {
+    // Scaling items for things that are ran in web browser
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.sacle.pageAlignVertically = true;
+    // creating the paddle
+    game.load.image("paddle", "assets/Paddle/50-breakout-Tiles.png")
     this.load.image('ball', 'assets/Ball/Ball.png');
     this.load.image('yellowBrick', 'assets/Bricks/Yellow1.png');
     this.load.image('redBrick', 'assets/Bricks/Red1.png');
-
-
-
+ 
+    // note this might need to be game instead of this need to research but ran out of time
+ 
+ 
   } // Basic function to preload the assest
   
   
   function create() {
-
+    //might need to add physics items here outisde of the items being loaded
+ 
+    // Creating the paddle
+ 
+    paddle = game.add.sprite(
+        game.world.width * 0.5, // this is how to position the paddle
+        game.world.height - 5,
+        "paddle",
+    );
+    paddle.anchor.set(0.5,1);
+    game.physics.enable(paddle, Phaser.Physics.arcade);
+ 
+ 
     yellowBricks = this.physics.add.group({
         key: 'yellowBrick',
         repeat: 9,
@@ -54,9 +73,14 @@ function preload() {
         }
     });
     
-
+ 
   } // basic funtion that runs when everything is ready
   
   
   
-  function update() {} // Basic function to update the frame NOTE* I kept these the same so for simplicity
+  function update() {
+    game.physics.arcade.collide(ball, paddle);
+    // method of allowing paddle to move but will need to look into on sunday more
+    paddle.x = game.input.x || game.world.wodth*0.5;
+ 
+  } // Basic function to update the frame NOTE* I kept these the same so for simplicity
